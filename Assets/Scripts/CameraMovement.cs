a621ff1effc64fba3ea4d9d729ad1ace42cb4268
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2020-01-31 21:09:46 -0500

    created player movement, jump and run faster

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] float offsetY = 0;
+    [SerializeField] float interpolate = 0.2f; 
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, player.transform.position + new Vector3(0, offsetY, 0), .2f);
+    }
+}
