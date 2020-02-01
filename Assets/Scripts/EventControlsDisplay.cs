e12b8fda996646bd65898d1aca59973706e17f33
Giovani Rodriguez <giovaniluisrodriguez@gmail.com>
2020-02-01 11:44:43 -0500

    Finished event control displaying and movement

--- Assets/Scripts/EventControlsDisplay.cs
+++ Assets/Scripts/EventControlsDisplay.cs
@@ -8,10 +8,13 @@ public class EventControlsDisplay : MonoBehaviour
     [SerializeField] private Camera camera;
     [SerializeField] private Tile tile;
     [SerializeField] private float framesPerControlTile = 15;
-    [SerializeField] private int nextControlYOffset = 1;
-    [SerializeField] private int nextControlXOffset = -2;
+    [SerializeField] private float nextControlYOffset = 0;
+    [SerializeField] private float nextControlXOffset = 0;
 
     private Tilemap tilemap;
+    private int maxNumberOfControlTiles = 3;
+    private int numberOfControlTilesSet = 0;
+    private Vector3Int? initialCameraPosition;
     private int nextControlAccumalator = 0;
 
     private int framePerSecond => 60;
@@ -23,19 +26,28 @@ public class EventControlsDisplay : MonoBehaviour
 
     private void Update()
     {
-
-        if (nextControlAccumalator >= framesPerControlTile && nextControlXOffset < 1)
-        {
-            tilemap.SetTile(GetCameraPosition() + new Vector3Int(nextControlXOffset, nextControlYOffset, 0), tile);
-            nextControlAccumalator = 0;
-            nextControlXOffset++;
-        }
-        else
-        {
-            //transform.position = Vector2.Lerp(transform.position, player.transform.position + new Vector3(0, offsetY, 0), .1f);
-        }
-
+        SetTile();
+        SetTileMapPosition();
+        
         nextControlAccumalator++;
+    }
+
+    private void SetTile()
+    {
+        if (nextControlAccumalator >= framesPerControlTile && numberOfControlTilesSet < maxNumberOfControlTiles)
+        {
+            if (initialCameraPosition == null)
+                initialCameraPosition = GetCameraPosition();
+
+            tilemap.SetTile(GetNextTilePosition(), tile);
+
+            nextControlAccumalator = 0;
+            numberOfControlTilesSet++;
+        }
+        else if (numberOfControlTilesSet == maxNumberOfControlTiles)
+        {
+            initialCameraPosition = null;
+        }
     }
 
     private Vector3Int GetCameraPosition()
@@ -43,5 +55,17 @@ public class EventControlsDisplay : MonoBehaviour
         return new Vector3Int((int) camera.transform.position.x, 
                               (int) camera.transform.position.y, 
                               (int) camera.transform.position.z);
+    }
+
+    private Vector3Int GetNextTilePosition()
+    {
+        return (initialCameraPosition + new Vector3Int(numberOfControlTilesSet, 0, 0))
+               .GetValueOrDefault();
+    }
+
+    private void SetTileMapPosition()
+    {
+        transform.position = camera.transform.position + 
+                             new Vector3(nextControlXOffset, nextControlYOffset, 0); // offset
     }
 }
