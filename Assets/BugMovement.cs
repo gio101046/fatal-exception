e7883786f84b4f1282d56dc01a435bd3e5176716
Jack Rus <enagimov@gmail.com>
2020-01-31 21:21:14 -0500

    minor

--- Assets/BugMovement.cs
+++ Assets/BugMovement.cs
@@ -6,39 +6,15 @@ public class BugMovement : MonoBehaviour
 {
     public LayerMask layerMask;
     public float speed;
+    private bool movingRight = true;
     Rigidbody2D bugBody;
-    Transform bugTrans;
-    float bugWidth;
+    private Transform groundDetection;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        bugTrans = this.transform;
-        bugBody = this.GetComponent<Rigidbody2D>();
-        bugWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        // CHECK IF GROUNDED
-        //Vector2 lineCastPos = bugTrans.position - bugTrans.right * bugWidth;
-
-        //bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, layerMask);
-
-        //if (!isGrounded)
-        //{
-        //    Vector3 currentRotation = bugTrans.eulerAngles;
-        //    currentRotation.y += 180;
-        //    bugTrans.eulerAngles = currentRotation;
-        //}
-
-
-        // Move Forward
-        Vector2 bugVelocity = bugBody.velocity;
-        bugVelocity.x = speed;
-        bugBody.velocity = bugVelocity;
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
     void FixedUpdate()
