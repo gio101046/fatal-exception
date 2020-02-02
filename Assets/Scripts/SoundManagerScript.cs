68cf1b9c9f99f73248f74b824f20a7d9faa99169
chyknkat <kortiz75@gmail.com>
2020-02-01 21:30:59 -0500

    add scream and wrong button to assets and sound manager

--- Assets/Scripts/SoundManagerScript.cs
+++ Assets/Scripts/SoundManagerScript.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SoundManagerScript : MonoBehaviour
 {
 
-    public static AudioClip jumpSound;
+    public static AudioClip jumpSound, screamSound, wrongButtonSound;
     static AudioSource audioSrc;
 
     // Start is called before the first frame update
     void Start()
     {
         jumpSound = Resources.Load<AudioClip>("Audio/Jump");
+        jumpSound = Resources.Load<AudioClip>("Audio/GirlyScream");
+        jumpSound = Resources.Load<AudioClip>("Audio/WrongButton");
 
         audioSrc = GetComponent<AudioSource>();
     }
@@ -28,6 +30,12 @@ public class SoundManagerScript : MonoBehaviour
         {
             case "jump":
                 audioSrc.PlayOneShot(jumpSound);
+                break;
+            case "scream":
+                audioSrc.PlayOneShot(screamSound);
+                break;
+            case "wrong":
+                audioSrc.PlayOneShot(wrongButtonSound);
                 break;
         }
     }
